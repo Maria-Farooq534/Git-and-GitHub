66ffcee48bece3b92c53d1b524625ee6f2107ab7
Maria-Farooq534 <mariafarooq164@gmail.com>
2025-07-24 10:51:57 +0500

    for loop

--- python.py
+++ python.py
@@ -1,9 +1,14 @@
-print("Hello, World")
+# print("Hello, World")
 
-def sum(a,b):
-    sum = a+b
-    return sum
+# def sum(a,b):
+#     sum = a+b
+#     return sum
 
 
-sum1 = sum(3, 6)
-print(sum1)
+# sum1 = sum(3, 6)
+# print(sum1)
+
+for i in range(5):
+    
+    for start in range(5):
+        print(f"({i} , {start})")
